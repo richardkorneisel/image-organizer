6330a3f1835d6b85bde6b0aa7d9309721ae87371
Rich Korneisel <korneisel@cfu.net>
2021-01-28 16:47:11 -0600

    basic css works

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/pokemon', pokemon)
