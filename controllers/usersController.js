d9b8746a3d821382b8b74c3afbb4215d74af5609
Rich Korneisel <korneisel@cfu.net>
2021-02-04 20:16:29 -0600

    Pokemon 5 Complete

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,8 @@
 const express = require("express");     //still using express
 const router = express.Router();        //need express router
 
-const users = require("../users");   //location of model(data)
-
+// const users = require("../users");   //location of model(data)
+const User = require('../models').User;
 //Index (Home)page
 router.get('/', (req, res) => {                 
     res.render('users/index.ejs')     
@@ -15,41 +15,85 @@ router.get("/signup", (req, res) => {
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
+
+// router.post("/login", (req, res) => {
+//     let thisUser = users.findIndex((user)=>   // Search for which user is logging in
+//     user.username === req.body.username && user.password === req.body.password
+//     )
+//     res.redirect('/users/profile/' + thisUser)   
+//   });  
+// Serialized login 
 router.post("/login", (req, res) => {
-    let thisUser = users.findIndex((user)=>   // Search for which user is logging in
-    user.username === req.body.username && user.password === req.body.password
-    )
-    res.redirect('/users/profile/' + thisUser)   
-  });  
+  User.findOne({
+      where: {
+          username: req.body.username,
+          password: req.body.password
+      }
+  }) .then((thisUser)=>{
+      console.log(thisUser);
+      res.redirect('/users/profile/' + thisUser.id) 
+    })
+    
+});  
 // Post to profile file, need to create 
-router.post("/profile", (req, res) => {
-    users.push(req.body);
-    console.log(req.body); //to check
-    console.log(users);    //to check
-    let userIndex = users.length - 1  //target end of users array
-    res.redirect(`profile/${userIndex}`);
-  });  
-// Used '/:index/edit', from fruits to it was closest
-router.get('/profile/:index', (req, res) =>{   //need to have index, referenced below    
+// router.post("/profile", (req, res) => {
+//     users.push(req.body);
+//     console.log(req.body); //to check
+//     console.log(users);    //to check
+//     let userIndex = users.length - 1  //target end of users array
+//     res.redirect(`profile/${userIndex}`);
+//   });  
+router.post('/profile', (req, res)=>{
+    User.create(req.body).then((newUser) => {
+      res.redirect("profile/" + newUser.id);
+    });
+  });
+
+  // Used '/:index/edit', from fruits to it was closest
+// router.get('/profile/:index', (req, res) =>{   //need to have index, referenced below    
+//     res.render('users/profile.ejs', { 
+// 		userInfo: users[req.params.index], 
+// 		index: req.params.index
+// 	});
+// });
+// SerializedUsed '/:index/edit', from fruits to it was closest
+router.get('/profile/:id', (req, res) =>{   //need to have index, referenced below    
+    User.findByPk(req.params.id).then((thisUser)=> {
     res.render('users/profile.ejs', { 
-		userInfo: users[req.params.index], 
-		index: req.params.index
+		userInfo: thisUser, 
+        index: thisUser.id
+        });
 	});
 });
 // Used above for template
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
-router.put('/profile/:index', (req, res) => { 
-    users[req.params.index] = req.body;
-    let index = req.params.index
-    console.log(users) 
-	res.redirect('/users/profile/' + index); 
-});
-router.delete('/:index', (req, res) => {
-    users.splice(req.params.index, 1); 
-    console.log(users)
-    res.redirect('/users');  
-});
+// router.put('/profile/:id', (req, res) => { 
+//     users[req.params.index] = req.body;
+//     let index = req.params.index
+//     console.log(users) 
+// 	res.redirect('/users/profile/' + index); 
+// });
+router.put("/profile/:id", (req, res) => {
+    User.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+    }).then((user) => {
+      res.redirect("/users/profile/" + req.params.id);
+    });
+  });
+
+// router.delete('/:id', (req, res) => {
+//     users.splice(req.params.index, 1); 
+//     console.log(users)
+//     res.redirect('/users');  
+// });
+router.delete('/:id', (req, res) =>{
+    User.destroy({ where: { id: req.params.id } }).then(() => {
+      res.redirect("/users");
+    });
+  }); 
+
 
 module.exports = router; //required, put on bottom
